230a8e64f4b3ee30cebf95012f36e30bde59c3ac
Sidibe8 <yorosidibe@arroebaze.com>
2025-09-06 13:51:14 +0000

    vercel ssa

--- src/server.js
+++ src/server.js
@@ -100,9 +100,10 @@ app.get("/", (req, res) => {
   res.send("Bienvenue ");
 });
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () =>
-//   console.log(`Serveur lancé sur http://localhost:${PORT}`)
-// );
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+  console.log(`Serveur lancé sur http://localhost:${PORT}`)
+);
 
-module.exports = app;
+// module.exports = app;
+//
